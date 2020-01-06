daedaleanai/reqtraq

commit 5329de06da6bb444c6e5ff920e12570e111f1705

Allow linking between reqs and code functions

report.go
@@ -94,24 +94,24 @@ var reportTmplText = `
  	{{end}}
 {{ end }}
 
-{{ define "CODEFILES"}}
-	<p>Code Files:
+{{ define "CODETAGS"}}
+	{{ if . }}
+		<p>Code:
 		{{ range . }}
-			<a href="file://{{ .Path }}" target="_blank">{{ .ID }}</a>
-		{{ else }}
-			<span class="text-danger">No code files</span>
+			<a href="{{ .URL }}" target="_blank">{{ .Path }}:{{ .Tag }}</a>
 		{{ end }}
-	</p>
+		</p>
+	{{ end }}
 {{ end }}
 
 {{ define "CHANGELIST" }}
-	<p>Changelists:
-		{{ range $k, $v := . }}
-			<a href="{{ $v }}" target="_blank"><span class="label label-primary">{{ $k }}</span></a>
-		{{ else }}
-			<span class="text-danger">No changelist</span>
-		{{ end }}
-	</p>
+	{{ if . }}
+		<p>Changelists:
+			{{ range $k, $v := . }}
+				<a href="{{ $v }}" target="_blank"><span class="label label-primary">{{ $k }}</span></a>
+			{{ end }}
+		</p>
+	{{ end }}
 {{ end }}
 
 {{ define "STATUSFIELD" }}
@@ -143,9 +143,11 @@ var reportTmplText = `
 							<ul>
 							{{ range .Children }}
 								<li>
-									{{ template "REQUIREMENT" ($.Once.Once .) }}
-									{{ template "CODEFILES" .Children }}
-									{{ template "CHANGELIST" .Changelists }}
+									{{ with ($.Once.Once .) }}
+										{{ template "REQUIREMENT" . }}
+										{{ template "CODETAGS" .Tags }}
+										{{ template "CHANGELIST" .Changelists }}
+									{{ end }}
 								</li>
 							{{ else }}
 								<li class="text-danger">No children</li>
@@ -163,43 +165,51 @@ var reportTmplText = `
 	</ul>
 	{{template "FOOTER"}}
 {{end}}
+
 {{define "BOTTOMUP"}}
 	{{template "HEADER"}}
 	<h1>Bottom Up Tracing</h1>
 	
 	<ul style="list-style: none; padding: 0; margin: 0;">
-		{{ range .Reqs.CodeFilesByPosition }}
+		{{ range .Reqs.CodeFiles }}
 			<li>
-				<h3><a href="{{ .Path }}" target="_blank">{{ .ID }}</a></h3>
-				{{ template "STATUSFIELD" . }}
+				<h3><a href="{{ .URL }}" target="_blank">{{ .Path }}:{{ .Tag }}</a></h3>
+
 				<!-- LLRs -->
 				<ul>
-				{{ range .Parents }}
+					{{ range .Parents }}
+					{{ with ($.Once.Once .) }}
 					<li>
-						{{ template "REQUIREMENT" ($.Once.Once .) }}
+						{{ template "REQUIREMENT" . }}
 						{{ template "CHANGELIST" .Changelists }}
 
 						<!-- HLRs -->
-							<ul>
+						<ul>
 							{{ range .Parents }}
-								<li>
-									{{ template "REQUIREMENT" ($.Once.Once .) }}
-									<!-- SYSTEM -->
-									<ul>
+							{{ with ($.Once.Once .) }}
+							<li>
+								{{ template "REQUIREMENT" . }}
+
+								<!-- SYSTEM -->
+								<ul>
 									{{ range .Parents }}
-										<li>
-											{{ template "REQUIREMENT" ($.Once.Once .) }}
-										</li>
+									{{ with ($.Once.Once .) }}
+									<li>
+										{{ template "REQUIREMENT" . }}
+									</li>
+									{{ end }}
 									{{ else }}
 										<li class="text-danger">No parents</li>
 									{{ end }}
-									</ul>
-								</li>
+								</ul>
+							</li>
+							{{ end }}
 							{{ else }}
 								<li class="text-danger">No parents</li>
 							{{ end }}
-							</ul>
+						</ul>
 					</li>
+					{{ end }}
 					{{ else }}
 						<li class="text-danger">No parents</li>
 					{{ end }}
@@ -224,17 +234,6 @@ var reportTmplText = `
 		</li>
 	{{ else }}
 		<li class="text-success">No basic errors found.</li>
-	{{ end }}
-	</ul>
-
-	<h3>Dangling Requirements</h3>
-	<ul>
-	{{ range .Reqs.DanglingReqsByPosition }}
-		<li>
-			{{ template "REQUIREMENT" ($.Once.Once .) }}
-		</li>
-	{{ else }}
-		<li class="text-success">No dangling HLRs or LLRs found.</li>
 	{{ end }}
 	</ul>
 
@@ -274,10 +273,11 @@ var reportTmplText = `
 				{{ if .Matches $.Filter $.Diffs }}{{ template "REQUIREMENT" ($.Once.Once .) }}{{ end }}
 				{{ range .Children }}
 					{{ if .Matches $.Filter $.Diffs }}
-						{{ template "REQUIREMENT" ($.Once.Once .) }}
-						{{ template "CODEFILES" .Children }}
-						{{ template "CHANGELIST" .Changelists }}
-
+						{{ with ($.Once.Once .) }}
+							{{ template "REQUIREMENT" . }}
+							{{ template "CODETAGS" .Tags }}
+							{{ template "CHANGELIST" .Changelists }}
+						{{ end }}
 					{{ end }}
 				{{ end }}
 			{{ end }}
@@ -295,10 +295,11 @@ var reportTmplText = `
 		{{ range .Reqs.CodeFilesByPosition }}
 			{{ range .Parents }}
 				{{ if .Matches $.Filter $.Diffs }}
-					{{ template "REQUIREMENT" ($.Once.Once .) }}
-					{{ template "CODEFILES" .Children }}
-					{{ template "CHANGELIST" .Changelists }}
-
+					{{ with ($.Once.Once .) }}
+						{{ template "REQUIREMENT" . }}
+						{{ template "CODETAGS" .Tags }}
+						{{ template "CHANGELIST" .Changelists }}
+					{{ end }}
 				{{ end }}
 				{{ range .Parents }}
 					{{ if .Matches $.Filter $.Diffs }}{{ template "REQUIREMENT" ($.Once.Once .) }}{{ end }}

code.go
@@ -1,0 +1,213 @@
+package main
+
+import (
+	"bufio"
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/daedaleanai/reqtraq/git"
+	"github.com/daedaleanai/reqtraq/linepipes"
+	"github.com/pkg/errors"
+)
+
+// SourceCodeFileExtensions are listed by language.
+// To see the available languages, run: ctags --list-languages
+var SourceCodeFileExtensions = map[string][]string{
+	"C":   {".c", ".h"},
+	"C++": {".cc", ".hh"},
+	"GO":  {".go"},
+}
+
+const InstallUniversalCtags = "Install as described in https://github.com/universal-ctags/ctags#the-latest-build-and-package"
+
+// runCtags executes Universal Ctags with the specified arguments.
+func runCtags(args ...string) (lines chan string, errors chan error) {
+	ctags, ok := os.LookupEnv("REQTRAQ_CTAGS")
+	if !ok {
+		ctags = "ctags"
+	}
+	return linepipes.Run(ctags, args...)
+}
+
+// checkCtagsAvailable returns an error when Universal Ctags cannot be found.
+func checkCtagsAvailable() error {
+	out, err := linepipes.All(runCtags("--version"))
+	if err != nil {
+		return errors.Wrap(err, "universal-ctags not available. "+InstallUniversalCtags)
+	}
+	if !strings.Contains(out, "Universal Ctags") {
+		return fmt.Errorf("`ctags` tool is not universal-ctags. " + InstallUniversalCtags)
+	}
+	return nil
+}
+
+// parseTags returns the tags found by ctags.
+func parseTags(lines chan string) ([]Code, error) {
+	res := make([]Code, 0)
+	for line := range lines {
+		parts := strings.Split(line, "\t")
+		if len(parts) < 4 {
+			// Most probably some lines with metadata info about
+			// the ctags generator.
+			continue
+		}
+		tag := parts[0]
+		p := parts[1]
+		if !IsSourceCodeFile(p) {
+			continue
+		}
+		relativePath, err := relativePathToRepo(p, git.RepoPath())
+		if err != nil {
+			return nil, errors.Wrap(err, fmt.Sprintf("failed to parse path on line: %v", parts))
+		}
+		if !strings.HasPrefix(parts[3], "line:") {
+			return nil, fmt.Errorf("line number unknown prefix: %v", parts)
+		}
+		line, err := strconv.Atoi(parts[3][5:])
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse line number: %v", parts)
+		}
+		res = append(res, Code{Path: relativePath, Tag: tag, Line: line})
+	}
+	return res, nil
+}
+
+// parseFileComments detects comments in the specified source code file and
+// associates them with the tags detected in the same file.
+// Returns a hash for the file
+func parseFileComments(absolutePath string, tags []Code) (string, error) {
+	f, err := os.Open(absolutePath)
+	if err != nil {
+		return "", err
+	}
+	h := sha1.New()
+	// git compatible hash
+	if s, err := f.Stat(); err == nil {
+		fmt.Fprintf(h, "blob %d", s.Size())
+		h.Write([]byte{0})
+	}
+
+	// Detect comments.
+	comments := make(map[int][]string, 0)
+	scanner := bufio.NewScanner(io.TeeReader(f, h))
+	var comment []string
+	var lineNumber int
+	for scanner.Scan() {
+		lineNumber++
+		line := strings.TrimSpace(scanner.Text())
+		if strings.HasPrefix(line, "//") || strings.HasPrefix(line, "*/") || strings.HasPrefix(line, "/*") || strings.HasPrefix(line, "* ") {
+			if comment == nil {
+				comment = make([]string, 0)
+			}
+			comment = append(comment, line)
+		} else {
+			if comment != nil {
+				// This is the first line after the comment.
+				comments[lineNumber] = comment
+				comment = nil
+			}
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return "", errors.Wrap(err, "failed to read source code file")
+	}
+
+	// Associate the detected comments with the tags.
+	for i := range tags {
+		if comment, ok := comments[tags[i].Line]; ok {
+			tags[i].Comment = comment
+		}
+	}
+
+	return string(h.Sum(nil)), nil
+}
+
+// tagCode runs ctags over the specified directory and parses the generated
+// tags file.
+func tagCode(codePath string) (map[string][]Code, error) {
+	absoluteCodePath := filepath.Join(git.RepoPath(), codePath)
+	languages := make([]string, 0, len(SourceCodeFileExtensions))
+	for l := range SourceCodeFileExtensions {
+		languages = append(languages, l)
+	}
+	lines, errs := runCtags(
+		// We're only interested in a limited set of languages.
+		// Avoid scanning JSON, Markdown, etc.
+		"--languages="+strings.Join(languages, ","),
+		// To see the available kinds for a language: ctags --list-kinds-full=C++
+		// We're interested only in functions.
+		"--kinds-C=f",
+		"--kinds-C++=f",
+		"--kinds-GO=f",
+		// To see the available fields: ctags --list-fields
+		// We need the line number.
+		"--fields=n",
+		"--recurse", "-f", "-", absoluteCodePath)
+
+	tags, err := parseTags(lines)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse ctags output")
+	}
+
+	if err, _ := <-errs; err != nil {
+		return nil, errors.Wrap(err, "failed to run ctags to find methods in the source code")
+	}
+
+	tagsByFile := make(map[string][]Code, 0)
+	for _, tag := range tags {
+		_, ok := tagsByFile[tag.Path]
+		if !ok {
+			tagsByFile[tag.Path] = make([]Code, 0)
+		}
+		tagsByFile[tag.Path] = append(tagsByFile[tag.Path], tag)
+	}
+	return tagsByFile, nil
+}
+
+func IsSourceCodeDir(dirPath string) bool {
+	return path.Base(dirPath) != "testdata"
+}
+
+func IsSourceCodeFile(name string) bool {
+	ext := strings.ToLower(path.Ext(name))
+	for _, extensions := range SourceCodeFileExtensions {
+		for _, e := range extensions {
+			if e == ext {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// parseComments updates the specified tags with the discovered code comments.
+func (rg *reqGraph) parseComments(absoluteCodePath string) error {
+	// Walk through the code.
+	err := filepath.Walk(absoluteCodePath, func(filePath string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			if !IsSourceCodeDir(filePath) {
+				return filepath.SkipDir
+			}
+		} else {
+			if !IsSourceCodeFile(info.Name()) {
+				return nil
+			}
+
+			p, err := relativePathToRepo(filePath, git.RepoPath())
+			if err != nil {
+
+			}
+			if _, err := parseFileComments(filePath, rg.CodeTags[p]); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	return err
+}

code_test.go
@@ -1,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/daedaleanai/reqtraq/config"
+	"github.com/daedaleanai/reqtraq/git"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCheckCtagsAvailable(t *testing.T) {
+	if err := checkCtagsAvailable(); err != nil {
+		t.Fatal(errors.Wrap(err, "ctags not available - REQTRAQ_CTAGS env var can be set to specify location"))
+	}
+}
+
+type TagMatch struct {
+	tag     string
+	line    int
+	comment string
+}
+
+func LookFor(t *testing.T, sourceFile string, tagsPerFile map[string][]Code, expectedTags []TagMatch) {
+	tags, ok := tagsPerFile[sourceFile]
+	assert.True(t, ok)
+	assert.Equal(t, 3, len(tags))
+
+	for _, tag := range tags {
+		found := false
+		for _, e := range expectedTags {
+			if e.tag == tag.Tag {
+				found = true
+				assert.Equal(t, e.line, tag.Line)
+				assert.Equal(t, e.tag, tag.Tag)
+				if e.comment != "" {
+					assert.Equal(t, e.comment, strings.Join(tag.Comment, "\n"))
+				}
+				break
+			}
+		}
+		assert.True(t, found)
+	}
+}
+
+func TestTagCode(t *testing.T) {
+	tags, err := tagCode("testdata/cproject1")
+	if !assert.NoError(t, err) {
+		fmt.Println(tags)
+		return
+	}
+	assert.Equal(t, 1, len(tags))
+
+	expectedTags := []TagMatch{
+		{"enumerateObjects",
+			30,
+			""},
+		{"getSegment",
+			20,
+			""},
+		{"getNumberOfSegments",
+			14,
+			""},
+	}
+	LookFor(t, "testdata/cproject1/a.c", tags, expectedTags)
+}
+
+func TestReqGraph_ParseComments(t *testing.T) {
+	tags, err := tagCode("testdata/cproject1")
+	if !assert.NoError(t, err) {
+		fmt.Println(tags)
+		return
+	}
+
+	rg := reqGraph{Reqs: make(map[string]*Req, 0), CodeTags: tags}
+	err = rg.parseComments(filepath.Join(git.RepoPath(), "testdata/cproject1"))
+	if !assert.NoError(t, err) {
+		fmt.Println(tags)
+		return
+	}
+
+	expectedTags := []TagMatch{
+		{"enumerateObjects",
+			30,
+			`// This method does stuff also.
+// @llr REQ-PROJ-SWL-13
+// @xlr R-1`},
+		{"getSegment",
+			20,
+			`// This method does stuff.
+// @llr REQ-PROJ-SWL-12`},
+		{"getNumberOfSegments",
+			14,
+			`// @llr REQ-PROJ-SWH-11`},
+	}
+	LookFor(t, "testdata/cproject1/a.c", tags, expectedTags)
+
+	rg.Reqs["REQ-PROJ-SWL-13"] = &Req{Level: config.LOW}
+	rg.Reqs["REQ-PROJ-SWH-11"] = &Req{Level: config.HIGH}
+	errs := SortErrs(rg.resolveCodeTags(nil))
+	assert.Equal(t, 2, len(errs))
+	assert.Equal(t, "Invalid reference in file testdata/cproject1/a.c function getNumberOfSegments: REQ-PROJ-SWH-11 must be a Software Low-Level Requirement.", errs[0])
+	assert.Equal(t, "Invalid reference in file testdata/cproject1/a.c function getSegment: REQ-PROJ-SWL-12 does not exist.", errs[1])
+}

config/config_ddln.go
@@ -11,7 +11,6 @@ const (
 	SYSTEM RequirementLevel = iota
 	HIGH
 	LOW
-	CODE
 )
 
 // Document types:

diff.go
@@ -92,8 +92,6 @@ func (r *Req) ChangedSince(pr *Req) (diffs []string) {
 
 	if r.Path != pr.Path {
 		diffs = append(diffs, fmt.Sprintf("File in which found changed from %q to %q", pr.Path, r.Path))
-	} else if r.FileHash != pr.FileHash {
-		diffs = append(diffs, fmt.Sprintf("File %q contents changed", r.Path))
 	}
 
 	var keys []string

req.go
@@ -8,11 +8,9 @@ package main
 
 import (
 	"bufio"
-	"crypto/sha1"
-	"errors"
 	"fmt"
 	"html/template"
-	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"path"
@@ -25,6 +23,7 @@ import (
 	"github.com/daedaleanai/reqtraq/config"
 	"github.com/daedaleanai/reqtraq/git"
 	"github.com/daedaleanai/reqtraq/linepipes"
+	"github.com/pkg/errors"
 )
 
 type RequirementStatus int
@@ -50,24 +49,27 @@ var (
 )
 
 // Req represenents a Requirement Node in the graph of Requirements.
-// The Attributes map has potential elements;
+// The Attributes map has potential elements:
 //  rationale safety_impact verification urgent important mode provenance
 type Req struct {
-	ID        string // code files do not have an ID, use Path as primary key
-	IDNumber  int    // the number contained in the ID, if any.
-	Level     config.RequirementLevel
-	Path      string // certification document or code file this was found in relative to repo root
-	FileHash  string // for code files, the sha1 of the contents
+	ID       string
+	IDNumber int
+	Level    config.RequirementLevel
+	// Path identifies the file this was found in relative to repo root.
+	Path      string
 	ParentIds []string
-	Parents   []*Req
-	Children  []*Req
-	Title     string
+	// Parents holds the parent requirements.
+	Parents []*Req
+	// Children holds the children requirements.
+	Children []*Req
+	// Tags holds the associated code functions.
+	Tags  []*Code
+	Title string
 	// Body contains various HTML tags (links, converted markdown, etc). Type must be HTML,
 	// not a string, so it's not HTML-escaped by the templating engine.
 	Body       template.HTML
 	Attributes map[string]string
 	Position   int
-	Seen       bool
 	Status     RequirementStatus
 }
 
@@ -81,30 +83,8 @@ func (r *Req) ReqType() string {
 	return parts[2]
 }
 
-func (r *Req) resolveUp() {
-	r.Seen = true
-	for _, p := range r.Parents {
-		p.resolveUp()
-	}
-}
-
-func (r *Req) resolveDown() RequirementStatus {
-	r.Seen = true
-	r.Status = COMPLETED
-	if r.Level != config.CODE && len(r.Children) == 0 {
-		r.Status = NOT_STARTED
-	} else {
-		for _, c := range r.Children {
-			if c.resolveDown() != COMPLETED {
-				r.Status = STARTED
-			}
-		}
-	}
-	return r.Status
-}
-
 // IsDeleted checks if the requirement title starts with 'DELETED'
-// @REQ-TRAQ-SWL-17
+// @llr REQ-TRAQ-SWL-17
 func (r *Req) IsDeleted() bool {
 	return strings.HasPrefix(r.Title, "DELETED")
 }
@@ -190,10 +170,49 @@ func changelistUrlsForFilepath(filepath string) []string {
 	return urls
 }
 
+// Code represents a Code Node in the graph of Requirements.
+type Code struct {
+	// Path is the code file this was found in relative to repo root.
+	Path string
+	// Tag is the name of the function.
+	Tag string
+	// Line number where the function starts.
+	Line int
+	// The comment above the function.
+	Comment []string
+	// FileHash is the sha1 of the contents.
+	FileHash  string
+	ParentIds []string
+	Parents   []*Req
+}
+
+var reLLRReference = regexp.MustCompile(fmt.Sprintf(`\s@llr (%s)\z`, reReqIdStr))
+
+func (code *Code) URL() string {
+	return fmt.Sprintf("/code/%s#L%d", code.Path, code.CommentLine())
+}
+
+func (code *Code) CommentLine() int {
+	return code.Line - len(code.Comment)
+}
+
+func (code *Code) ReqIDsInComment() []string {
+	refs := make([]string, 0)
+	for _, s := range code.Comment {
+		if parts := reLLRReference.FindStringSubmatch(s); len(parts) > 0 {
+			refs = append(refs, parts[1])
+		}
+	}
+	return refs
+}
+
 // A ReqGraph maps IDs and code files paths to Req structures.
 // @llr REQ-TRAQ-SWL-15
 type reqGraph struct {
+	// Reqs contains the requirements by ID.
 	Reqs map[string]*Req
+	// CodeTags contains the source code functions per file.
+	CodeTags map[string][]Code
 	// Errors which have been found while analyzing the graph.
 	// This is extended in multiple places.
 	Errors []error
@@ -205,9 +224,16 @@ type reqGraph struct {
 // The separate returned error specifies how reading the certdocs and code
 // failed, if it did.
 func CreateReqGraph(certdocsPath, codePath string) (*reqGraph, error) {
-	rg := &reqGraph{make(map[string]*Req, 0), make([]error, 0)}
+	rg := &reqGraph{make(map[string]*Req, 0), nil, make([]error, 0)}
 
-	err := filepath.Walk(filepath.Join(git.RepoPath(), certdocsPath),
+	filesListFile, err := ioutil.TempFile("", "list-*")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(filesListFile.Name())
+
+	// Walk through the documents.
+	err = filepath.Walk(filepath.Join(git.RepoPath(), certdocsPath),
 		func(fileName string, info os.FileInfo, err error) error {
 			switch strings.ToLower(path.Ext(fileName)) {
 			case ".md":
@@ -216,31 +242,27 @@ func CreateReqGraph(certdocsPath, codePath string) (*reqGraph, error) {
 					return err
 				}
 				rg.Errors = append(rg.Errors, parseErrs...)
+			case ".go":
+				if _, err := filesListFile.WriteString(fileName); err != nil {
+					return err
+				}
 			}
 			return nil
 		})
 	if err != nil {
-		return rg, fmt.Errorf("Failed walking certdocs: %v", err)
+		return rg, errors.Wrap(err, "failed walking certdocs")
 	}
-	// walk the code
-	err = filepath.Walk(filepath.Join(git.RepoPath(), codePath), func(fileName string, info os.FileInfo, err error) error {
-		switch strings.ToLower(path.Ext(fileName)) {
-		case ".cc", ".c", ".h", ".hh", ".go":
-			// TODO (pk,lb): do that in a nicer way without hard-coded folder names
-			if strings.Contains(codePath, "testdata") || !strings.Contains(fileName, "testdata") {
-				id := relativePathToRepo(fileName, git.RepoPath())
-				if id == "" {
-					log.Fatalf("Malformed code file path: %s not in %s", fileName, git.RepoPath())
-				}
-				if err := parseCode(id, fileName, rg); err != nil {
-					return err
-				}
-			}
-		}
-		return nil
-	})
+
+	// Tag the code.
+	absoluteCodePath := filepath.Join(git.RepoPath(), codePath)
+	rg.CodeTags, err = tagCode(codePath)
 	if err != nil {
-		return rg, fmt.Errorf("Failed walking code: %v", err)
+		return rg, errors.Wrap(err, "failed to tag code")
+	}
+
+	err = rg.parseComments(absoluteCodePath)
+	if err != nil {
+		return rg, errors.Wrap(err, "failed walking code")
 	}
 
 	rg.Errors = append(rg.Errors, rg.Resolve()...)
@@ -250,12 +272,20 @@ func CreateReqGraph(certdocsPath, codePath string) (*reqGraph, error) {
 
 // relativePathToRepo returns filePath relative to repoPath by
 // removing the path to the repository from filePath
-func relativePathToRepo(filePath, repoPath string) string {
-	fields := strings.SplitAfterN(filePath, repoPath, 2)
-	if len(fields) < 2 {
-		return ""
+func relativePathToRepo(filePath, repoPath string) (string, error) {
+	if filePath[:1] != "/" {
+		// Already a relative path.
+		return filePath, nil
 	}
-	return fields[1][1:] // omit leading slash
+	fields := strings.SplitN(filePath, repoPath, 2)
+	if len(fields) < 2 {
+		return "", fmt.Errorf("malformed code file path: %s not in %s", filePath, repoPath)
+	}
+	res := fields[1]
+	if res[:1] == "/" {
+		res = res[1:]
+	}
+	return res, nil
 }
 
 func (rg *reqGraph) AddReq(req *Req, path string) error {
@@ -271,7 +301,7 @@ func (rg *reqGraph) AddReq(req *Req, path string) error {
 func (rg *reqGraph) CheckAttributes(reportConf JsonConf, filter *ReqFilter, diffs map[string][]string) ([]error, error) {
 	var errs []error
 	for _, req := range rg.Reqs {
-		if req.Level != config.CODE && req.Matches(filter, diffs) {
+		if req.Matches(filter, diffs) {
 			errs = append(errs, req.CheckAttributes(reportConf.Attributes)...)
 		}
 	}
@@ -282,7 +312,7 @@ func (rg *reqGraph) CheckAttributes(reportConf JsonConf, filter *ReqFilter, diff
 func (rg *reqGraph) checkReqReferences(certdocPath string) ([]error, error) {
 	reParents := regexp.MustCompile(`Parents: REQ-`)
 
-	errors := make([]error, 0)
+	errs := make([]error, 0)
 
 	err := filepath.Walk(filepath.Join(git.RepoPath(), certdocPath),
 		func(fileName string, info os.FileInfo, err error) error {
@@ -301,9 +331,9 @@ func (rg *reqGraph) checkReqReferences(certdocPath string) ([]error, error) {
 					reqID := line[ids[0]:ids[1]]
 					v, reqFound := rg.Reqs[reqID]
 					if !reqFound {
-						errors = append(errors, fmt.Errorf("Invalid reference to inexistent requirement %s in %s:%d", reqID, fileName, lno))
+						errs = append(errs, fmt.Errorf("Invalid reference to inexistent requirement %s in %s:%d", reqID, fileName, lno))
 					} else if v.IsDeleted() && !discardRefToDeleted {
-						errors = append(errors, fmt.Errorf("Invalid reference to deleted requirement %s in %s:%d", reqID, fileName, lno))
+						errs = append(errs, fmt.Errorf("Invalid reference to deleted requirement %s in %s:%d", reqID, fileName, lno))
 					}
 				}
 			}
@@ -314,11 +344,7 @@ func (rg *reqGraph) checkReqReferences(certdocPath string) ([]error, error) {
 		return nil, err
 	}
 
-	return errors, nil
-}
-
-func (rg *reqGraph) AddCodeRefs(id, fileName, fileHash string, reqIds []string) {
-	rg.Reqs[fileName] = &Req{ID: id, Path: fileName, FileHash: fileHash, ParentIds: reqIds, Level: config.CODE}
+	return errs, nil
 }
 
 // @llr REQ-TRAQ-SWL-17
@@ -333,32 +359,19 @@ func (rg *reqGraph) Resolve() []error {
 			parent := rg.Reqs[parentID]
 			if parent != nil {
 				if parent.IsDeleted() && !req.IsDeleted() {
-					if req.Level != config.CODE {
-						errs = append(errs, errors.New("Invalid parent of requirement "+req.ID+": "+parentID+" is deleted."))
-					} else {
-						errs = append(errs, errors.New("Invalid reference in file "+req.Path+": "+parentID+" is deleted."))
-					}
+					errs = append(errs, errors.New("Invalid parent of requirement "+req.ID+": "+parentID+" is deleted."))
 				}
 				parent.Children = append(parent.Children, req)
 				req.Parents = append(req.Parents, parent)
 			} else {
-				if req.Level != config.CODE {
-					errs = append(errs, errors.New("Invalid parent of requirement "+req.ID+": "+parentID+" does not exist."))
-				} else {
-					errs = append(errs, errors.New("Invalid reference in file "+req.Path+": "+parentID+" does not exist."))
-				}
+				errs = append(errs, errors.New("Invalid parent of requirement "+req.ID+": "+parentID+" does not exist."))
 			}
 		}
 	}
 
+	errs = rg.resolveCodeTags(errs)
 	if len(errs) > 0 {
 		return errs
-	}
-
-	for _, req := range rg.Reqs {
-		if req.Level == config.SYSTEM {
-			req.resolveDown()
-		}
 	}
 
 	for _, req := range rg.Reqs {
@@ -366,13 +379,36 @@ func (rg *reqGraph) Resolve() []error {
 		sort.Sort(byPosition(req.Children))
 	}
 
-	for _, req := range rg.Reqs {
-		if req.Level == config.CODE {
-			req.resolveUp()
-			req.Position = req.Parents[0].Position
+	return nil
+}
+
+func (rg *reqGraph) resolveCodeTags(errs []error) []error {
+	for _, tags := range rg.CodeTags {
+		for i := range tags {
+			code := &tags[i]
+			code.ParentIds = code.ReqIDsInComment()
+			if len(code.ParentIds) == 0 {
+				errs = append(errs, errors.New("Function "+code.Tag+" in file "+code.Path+" has no parents."))
+			}
+			for _, parentID := range code.ParentIds {
+				parent := rg.Reqs[parentID]
+				if parent != nil {
+					if parent.IsDeleted() {
+						errs = append(errs, errors.New("Invalid reference in file "+code.Path+" function "+code.Tag+": "+parentID+" is deleted."))
+					}
+					if parent.Level == config.LOW {
+						parent.Tags = append(parent.Tags, code)
+						code.Parents = append(code.Parents, parent)
+					} else {
+						errs = append(errs, errors.New("Invalid reference in file "+code.Path+" function "+code.Tag+": "+parentID+" must be a Software Low-Level Requirement."))
+					}
+				} else {
+					errs = append(errs, errors.New("Invalid reference in file "+code.Path+" function "+code.Tag+": "+parentID+" does not exist."))
+				}
+			}
 		}
 	}
-	return nil
+	return errs
 }
 
 // OrdsByPosition returns the SYSTEM requirements which don't have any parent,
@@ -388,25 +424,14 @@ func (rg reqGraph) OrdsByPosition() []*Req {
 	return r
 }
 
-func (rg reqGraph) CodeFilesByPosition() []*Req {
-	var r []*Req
-	for _, v := range rg.Reqs {
-		if v.Level == config.CODE {
-			r = append(r, v)
+func (rg reqGraph) CodeFiles() []*Code {
+	var r []*Code
+	for filePath := range rg.CodeTags {
+		for i := range rg.CodeTags[filePath] {
+			r = append(r, &rg.CodeTags[filePath][i])
 		}
 	}
-	sort.Sort(byPosition(r))
-	return r
-}
-
-func (rg reqGraph) DanglingReqsByPosition() []*Req {
-	var r []*Req
-	for _, req := range rg.Reqs {
-		if !req.Seen && !req.IsDeleted() {
-			r = append(r, req)
-		}
-	}
-	sort.Sort(byPosition(r))
+	sort.Sort(byFilenameTag(r))
 	return r
 }
 
@@ -422,40 +447,24 @@ func (a byPosition) Len() int           { return len(a) }
 func (a byPosition) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a byPosition) Less(i, j int) bool { return a[i].Position < a[j].Position }
 
-var reLLRReference = regexp.MustCompile(fmt.Sprintf(`//\s*@llr\s*(%s).*`, reReqIdStr))
+type byFilenameTag []*Code
 
-func parseCode(id, fileName string, graph *reqGraph) error {
-	f, err := os.Open(fileName)
-	if err != nil {
-		return err
+func (a byFilenameTag) Len() int      { return len(a) }
+func (a byFilenameTag) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a byFilenameTag) Less(i, j int) bool {
+	switch strings.Compare(a[i].Path, a[j].Path) {
+	case -1:
+		return true
+	case 0:
+		return a[i].Tag < a[j].Tag
 	}
-	var refs []string
-	h := sha1.New()
-	// git compatible hash
-	if s, err := f.Stat(); err == nil {
-		fmt.Fprintf(h, "blob %d", s.Size())
-		h.Write([]byte{0})
-	}
-
-	scanner := bufio.NewScanner(io.TeeReader(f, h))
-	for scanner.Scan() {
-		if parts := reLLRReference.FindStringSubmatch(scanner.Text()); len(parts) > 0 {
-			refs = append(refs, parts[1])
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		return err
-	}
-	if len(refs) > 0 {
-		graph.AddCodeRefs(id, fileName, string(h.Sum(nil)), refs)
-	}
-	return nil
+	return false
 }
 
 func parseCertdocToGraph(fileName string, graph *reqGraph) ([]error, error) {
 	reqs, err := ParseCertdoc(fileName)
 	if err != nil {
-		return nil, fmt.Errorf("Error parsing %s: %v", fileName, err)
+		return nil, fmt.Errorf("error parsing %s: %v", fileName, err)
 	}
 
 	isReqPresent := make([]bool, len(reqs))

req_test.go
@@ -11,25 +11,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestReqGraph_AddCodeRefs(t *testing.T) {
-	rg := reqGraph{Reqs: make(map[string]*Req)}
-	const id = "certdocs/a.cc"
-	rg.AddCodeRefs(id, "a.cc", "", []string{"REQ-TRAQ-0-SWH-001"})
-	v := rg.Reqs["a.cc"]
-	if v == nil {
-		// fatal instead of error
-		t.Fatalf("Failure adding code reference %q: %v", id, rg)
-	}
-
-	if v.Level != config.CODE {
-		t.Errorf("expected level CODE, got %v", v.Level)
-	}
-
-	if v.Path != "a.cc" {
-		t.Errorf("expected path /tmp/a.cc, got %q", v.Path)
-	}
-}
-
 func TestReqGraph_AddReq(t *testing.T) {
 	rg := reqGraph{Reqs: make(map[string]*Req)}
 

commit d683a5d4ff31ba0ed2792297185f7e41b58d0223

Fix bottom-up filtered report
Was broken in 5329de06da6bb444c6e5ff920e12570e111f1705

report.go
@@ -296,7 +296,9 @@ var reportTmplText = `
 	
 	<h3><em>Filter Criteria: {{ $.Filter }} </em></h3>
 	<ul style="list-style: none; padding: 0; margin: 0;">
-		{{ range .Reqs.CodeFilesByPosition }}
+		{{ range .Reqs.CodeFiles }}
+		{{ with index $.Reqs.CodeTags . }}
+		{{ range . }}
 			{{ range .Parents }}
 				{{ if .Matches $.Filter $.Diffs }}
 					{{ with ($.Once.Once .) }}
@@ -312,6 +314,8 @@ var reportTmplText = `
 						{{ end }}
 				{{ end }}
 			{{ end }}
+		{{ end }}
+		{{ end }}
 		{{ end }}
 	</ul>
 	{{ template "FOOTER" }}