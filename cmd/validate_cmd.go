33998e74333e001e8c3eda59499525f1a222fed5
Armin Burgmeier <armin@daedalean.ai>
2024-02-23 17:12:40 +0100

    Fix diagnostics output for control/data flow related errors
    Before, these would just cause reqtraq to terminate immediately in a
    `log.Fatal`, without displaying any information on the control/data-flow
    related error.

--- cmd/validate_cmd.go
+++ cmd/validate_cmd.go
@@ -102,6 +102,21 @@ func buildJsonIssues(issues []diagnostics.Issue, jsonWriter *json.Encoder) error
 		case diagnostics.IssueTypeShallInRationale:
 			name = "Shall statement in rationale attribute"
 			code = "REQ14"
+		case diagnostics.IssueTypeInvalidFlowId:
+			name = "Invalid Flow ID"
+			code = "REQ15"
+		case diagnostics.IssueTypeFlowNotImplemented:
+			name = "Flow not implemented"
+			code = "REQ16"
+		case diagnostics.IssueTypeDuplicateFlowId:
+			name = "Duplicate Flow ID"
+			code = "REQ17"
+		case diagnostics.IssueTypeMissingFlowId:
+			name = "Missing Flow ID"
+			code = "REQ18"
+		case diagnostics.IssueTypeInvalidFlowDirection:
+			name = "Invalid flow direction"
+			code = "REQ19"
 		default:
 			log.Fatal("Unhandled IssueType: %r", issue.Type)
 		}
